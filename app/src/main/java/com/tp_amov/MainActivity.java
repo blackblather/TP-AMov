5e6d614df84330a7653f538be9e95b22295d00e1
joaom <joao.monteiro95@hotmail.com>
2019-11-17 18:51:55 +0000

    Initial Commit

---
+++ app/src/main/java/com/tp_amov/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tp_amov;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
