11e7ad42fa992b3a80532203e46963c6fc7ad386
joaom <joao.monteiro95@hotmail.com>
2019-11-21 05:19:17 +0000

    Button "Modo 3" enables/disables on network changes (works for mobile data only)

--- app/src/main/java/com/tp_amov/MainActivity.java
+++ app/src/main/java/com/tp_amov/MainActivity.java
@@ -1,5 +1,11 @@
 package com.tp_amov;
 
+import android.content.Context;
+import android.net.*;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkRequest networkRequest = new NetworkRequest.Builder().addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR).build();
+        connMgr.registerNetworkCallback(networkRequest, new ConnectivityManager.NetworkCallback(){
+            private Button btn_m3 = (Button) findViewById(R.id.btn_m3);
+            @Override
+            public void onAvailable(@NonNull Network network) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        btn_m3.setEnabled(true);
+                    }
+                });
+            }
+            @Override
+            public void onLost(@NonNull Network network) {
+                //runOnUiThread(() ->btn_m3.setEnabled(false));
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        btn_m3.setEnabled(false);
+                    }
+                });
+            }
+        });
     }
 }
