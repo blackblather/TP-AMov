9fde1459b811d95eeba31fc66bbe46e0654bc7bc
joaom <joao.monteiro95@hotmail.com>
2019-11-22 02:00:43 +0000

    "Main Activity" now calls "Select User Activity" and sends which button was clicked (sends button text)

--- app/src/main/java/com/tp_amov/MainActivity.java
+++ app/src/main/java/com/tp_amov/MainActivity.java
@@ -1,42 +1,60 @@
 package com.tp_amov;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.*;
+import android.view.View;
 import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.util.Consumer;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_SELECTED_MODE = "com.tp_amov.SELECTED_MODE";
+
+    public void OnModeBtnClick(View v){
+        //Clicked button is enabled
+        Intent intent = new Intent(this, SelectUserActivity.class);
+        String message = ((Button) v).getText().toString();
+        intent.putExtra(EXTRA_SELECTED_MODE, message);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ((Button) findViewById(R.id.btn_m3)).setEnabled(false); //By default, button is disabled
+        //By default, button is disabled
+        ((Button) findViewById(R.id.btn_m3)).setEnabled(false);
 
+        //Create listeners to change the "ENABLED" property of "btn_m3" based on network changes
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkRequest networkRequest = new NetworkRequest.Builder().addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR).build();
         connMgr.registerNetworkCallback(networkRequest, new ConnectivityManager.NetworkCallback(){
             private Button btn_m3 = (Button) findViewById(R.id.btn_m3);
             @Override
             public void onAvailable(@NonNull Network network) {
+                //May be called on startup
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         btn_m3.setEnabled(true);
                     }
-                }); //May be called on startup
+                });
             }
             @Override
             public void onLost(@NonNull Network network) {
+                //Is never called on startup
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         btn_m3.setEnabled(false);
                     }
-                }); //Is never called on startup
+                });
             }
         });
     }
--- app/src/main/java/com/tp_amov/SelectUserActivity.java
+++ app/src/main/java/com/tp_amov/SelectUserActivity.java
@@ -1,5 +1,7 @@
 package com.tp_amov;
 
+import android.content.Intent;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,10 @@ public class SelectUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_user);
+        Intent intent = getIntent();
+        String selectedMode = intent.getStringExtra(MainActivity.EXTRA_SELECTED_MODE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(selectedMode);
     }
 }
