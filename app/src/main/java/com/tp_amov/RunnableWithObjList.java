d7fe124f7d13f872e512267bac510f3013bb713f
joaom <joao.monteiro95@hotmail.com>
2019-12-06 17:17:49 +0000

    Removed unused interface

--- app/src/main/java/com/tp_amov/RunnableWithObjList.java
+++
@@ -1,7 +0,0 @@
-package com.tp_amov;
-
-import java.util.ArrayList;
-
-public interface RunnableWithObjList {
-    public abstract void run(Integer innerboard_index,Integer cell_index,Integer value);
-}
