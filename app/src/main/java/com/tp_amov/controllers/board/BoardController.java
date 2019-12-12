ddb0f6db5354c72b5efa88eb311196a51004dcd1
joaom <joao.monteiro95@hotmail.com>
2019-12-12 19:41:32 +0000

    Using board from webservice instead of preset

--- app/src/main/java/com/tp_amov/controllers/board/BoardController.java
+++ app/src/main/java/com/tp_amov/controllers/board/BoardController.java
@@ -65,8 +65,8 @@ public class BoardController extends ViewModel
                 @Override
                 public void onResponse(JSONObject response) {
                     try{
-                        //board = new Board(response);
-                        board = new Board(new JSONObject("{\"board\": [[0,0,1,0,0,0,0,0,0], [2,0,0,0,0,0,0,7,0], [0,7,0,0,0,0,0,0,0], [1,0,0,4,0,6,0,0,7], [0,0,0,0,0,0,0,0,0], [0,0,0,0,1,2,5,4,6], [3,0,2,7,6,0,9,8,0], [0,6,4,9,0,3,0,0,1], [9,8,0,5,2,1,0,6,0]]}"));
+                        board = new Board(response);
+//                        board = new Board(new JSONObject("{\"board\": [[0,0,1,0,0,0,0,0,0], [2,0,0,0,0,0,0,7,0], [0,7,0,0,0,0,0,0,0], [1,0,0,4,0,6,0,0,7], [0,0,0,0,0,0,0,0,0], [0,0,0,0,1,2,5,4,6], [3,0,2,7,6,0,9,8,0], [0,6,4,9,0,3,0,0,1], [9,8,0,5,2,1,0,6,0]]}"));
                         boardEvents.getOnBoardCreationSuccess().accept(board.toArray(Element.Type.defaultValue));
                     } catch (JSONException e) {
                         boardEvents.getOnBoardCreationError().run();
