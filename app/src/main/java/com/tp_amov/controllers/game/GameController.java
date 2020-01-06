4af5e61b6281e7f33401cd783ed190cfd76de9fb
joaom <joao.monteiro95@hotmail.com>
2020-01-06 05:53:50 +0000

    getting board from webservice

--- app/src/main/java/com/tp_amov/controllers/game/GameController.java
+++ app/src/main/java/com/tp_amov/controllers/game/GameController.java
@@ -120,11 +120,11 @@ public class GameController extends ViewModel
                 @Override
                 public void onResponse(JSONObject response) {
                     try{
-                        //board = new Board(response);
+                        board = new Board(response);
                         //UNSOLVED
                         //board = new Board(new JSONObject("{\"board\": [[0,0,1,0,0,0,0,0,0], [2,0,0,0,0,0,0,7,0], [0,7,0,0,0,0,0,0,0], [1,0,0,4,0,6,0,0,7], [0,0,0,0,0,0,0,0,0], [0,0,0,0,1,2,5,4,6], [3,0,2,7,6,0,9,8,0], [0,6,4,9,0,3,0,0,1], [9,8,0,5,2,1,0,6,0]]}"));
                         //SOLVED
-                        board = new Board(new JSONObject("{\"board\": [[9,8,4,5,7,2,6,1,3], [1,2,3,4,6,8,5,7,9], [5,6,7,1,3,9,2,4,8], [2,1,5,3,4,6,8,9,7], [3,4,6,8,9,7,1,2,5], [7,9,8,2,1,5,3,6,4], [4,3,2,7,5,1,9,8,6], [6,5,1,9,8,4,7,3,2], [8,7,9,6,2,3,4,5,1]]}"));
+                        //board = new Board(new JSONObject("{\"board\": [[9,8,4,5,7,2,6,1,3], [1,2,3,4,6,8,5,7,9], [5,6,7,1,3,9,2,4,8], [2,1,5,3,4,6,8,9,7], [3,4,6,8,9,7,1,2,5], [7,9,8,2,1,5,3,6,4], [4,3,2,7,5,1,9,8,6], [6,5,1,9,8,4,7,3,2], [8,7,9,6,2,3,4,5,1]]}"));
                         gameEvents.getOnBoardCreationSuccess().accept(board.toArray(Element.Type.defaultValue));
                     } catch (JSONException e) {
                         gameEvents.getOnBoardCreationError().run();
